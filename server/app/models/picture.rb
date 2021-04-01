5ffd4275d45f094b1ec517e38a2cd0f34ea5eb02
Zeragamba <Stephen-356@hotmail.com>
2021-04-01 10:51:24 -0400

    define as_json for Picture

--- server/app/models/picture.rb
+++ server/app/models/picture.rb
@@ -1,2 +1,11 @@
 class Picture < ApplicationRecord
+  def as_json(options)
+    {
+      "id" => self.id,
+      "data" => self.date,
+      "order" => self.order,
+      "title" => self.title,
+      "src" => "/api/picture/#{self.id}",
+    }
+  end
 end
