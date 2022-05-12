a7d6c3b1a5cff3141b78a90d4904718d8e7d9b07
Zeragamba <Zera@zeragamba.ca>
2022-05-12 18:40:57 -0400

    update packages

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,28 +1,88 @@
+/* global module */
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'plugins': [
+  plugins: [
     'react',
     '@typescript-eslint',
   ],
-  'rules': {
-    'react/prop-types': ['off'],
+  rules: {
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+        },
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          'order': 'asc',
+          'caseInsensitive': true,
+        },
+        'newlines-between': 'always',
+        warnOnUnassignedImports: true,
+        groups: [
+          ['builtin', 'external'],
+          ['parent', 'sibling'],
+        ],
+        pathGroups:
+          ['scss', 'sass', 'css', 'svg'].map(ext => ({
+            pattern: `*.${ext}`,
+            patternOptions: { matchBase: true },
+            group: 'sibling',
+            position: 'after',
+          })),
+      },
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 },
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
   },
-};
+}
