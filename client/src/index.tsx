367f89b7aac061f84b929dd0291e34a54d5ceab2
Zeragamba <Zera@zeragamba.ca>
2022-05-25 19:03:42 -0400

    update to react 18 renderer

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './App/App'
 import reportWebVitals from './report-web-vitals'
 
 import './index.scss'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+const rootEle = document.getElementById('root') as Element
+const root = createRoot(rootEle)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
