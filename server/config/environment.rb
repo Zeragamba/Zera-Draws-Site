4eb5727d8b8fee4031bfe4fd219c0c923542973a
Zeragamba <Zera@zeragamba.ca>
2022-10-13 13:35:35 -0400

    reconfigure docker compose files

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -7,5 +7,6 @@ Rails.application.initialize!
 AUTH_TOKEN_SECRET = ENV.fetch("TOKEN_SECRET")
 AUTH_TOKEN_ALGO = 'HS512'
 
-IMAGES_DIR = ENV.fetch("IMAGES_DIR")
-IMAGES_URL = ENV.fetch("IMG_SERVER_URL")
+PROJECT_ROOT = Rails.root.join('..')
+IMAGES_DIR = PROJECT_ROOT.join(ENV.fetch("IMAGES_DIR"))
+IMAGES_URL = ENV.fetch("IMAGES_URL")
