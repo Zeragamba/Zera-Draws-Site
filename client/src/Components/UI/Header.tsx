577fbdde77a9441d875a17f59d0b7975e041ca2c
Zeragamba <Zera@zeragamba.ca>
2022-10-15 10:46:05 -0400

    add link to header

--- client/src/Components/UI/Header.tsx
+++ client/src/Components/UI/Header.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { Glass } from './Glass'
 
 import styles from './Header.module.scss'
 
-export const Header: FC = () => (
-  <Glass className={styles.header}>
-    <header>
-      <div className={styles.headerText}>Zeragamba</div>
-    </header>
-  </Glass>
-)
+export const Header: FC = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Glass className={styles.header} onClick={() => navigate('/')}>
+      <header>Zeragamba</header>
+    </Glass>
+  )
+}
