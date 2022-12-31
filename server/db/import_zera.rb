f3b8aad24f64f2587d6efb41fa206da710a4fb34
Zeragamba <Zera@zeragamba.ca>
2022-12-30 20:36:13 -0500

    split zera seeds out

---
+++ server/db/import_zera.rb
@@ -0,0 +1,169 @@
+puts "Importing 2020-08-08-01.png"
+PictureManager.import(date: "2020-08-08", order: 0, title: "Ensign Twilight", filename: File.join(SEED_DATA_DIR, "2020-08-08-01.png"))
+  .add_tags("MLP/Twilight")
+  .add_tags("Project/MLP-Lower-Decks")
+  .add_tags("Show/Lower-Decks", "Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Traditional")
+
+puts "Importing 2020-08-08-02.png"
+PictureManager.import(date: "2020-08-08", order: 1, title: "Ensign Twilight (Coloured)", filename: File.join(SEED_DATA_DIR, "2020-08-08-02.png"))
+  .add_tags("MLP/Twilight")
+  .add_tags("Project/MLP-Lower-Decks")
+  .add_tags("Show/Lower-Decks", "Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Traditional")
+
+puts "Importing 2020-08-21-01.png"
+PictureManager.import(date: "2020-08-21", order: 0, title: "Ready to Fight", filename: File.join(SEED_DATA_DIR, "2020-08-21-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Traditional")
+
+puts "Importing 2020-08-22-01.png"
+PictureManager.import(date: "2020-08-22", order: 0, title: "Ensign Sunset", filename: File.join(SEED_DATA_DIR, "2020-08-22-01.png"))
+  .add_tags("MLP/Sunset-Shimmer")
+  .add_tags("Traditional")
+
+puts "Importing 2020-09-05-01.png"
+PictureManager.import(date: "2020-09-05", order: 0, title: "Roshia Ref", filename: File.join(SEED_DATA_DIR, "2020-09-05-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Fullcolour")
+
+puts "Importing 2020-09-26-01.png"
+PictureManager.import(date: "2020-11-26", order: 0, title: "Ensign Applejack", filename: File.join(SEED_DATA_DIR, "2020-09-26-01.png"))
+  .add_tags("MLP/Applejack")
+  .add_tags("Project/MLP-Lower-Decks")
+  .add_tags("Show/Lower-Decks", "Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Fullcolour")
+  .add_tags("Featured")
+
+puts "Importing 2020-10-01-01.jpg"
+PictureManager.import(date: "2020-10-01", order: 0, title: "Inktober 2020 | Fish", filename: File.join(SEED_DATA_DIR, "2020-10-01-01.jpg"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Project/Inktober2020")
+  .add_tags("Traditional")
+
+puts "Importing 2020-10-02-01.jpg"
+PictureManager.import(date: "2020-10-02", order: 0, title: "Inktober 2020 | Wisp", filename: File.join(SEED_DATA_DIR, "2020-10-02-01.jpg"))
+  .add_tags("Project/Inktober2020")
+  .add_tags("Traditional")
+
+puts "Importing 2020-10-03-01.jpg"
+PictureManager.import(date: "2020-10-03", order: 0, title: "Inktober 2020 | Bulky", filename: File.join(SEED_DATA_DIR, "2020-10-03-01.jpg"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Project/Inktober2020")
+  .add_tags("Traditional")
+
+puts "Importing 2020-10-04-01.jpg"
+PictureManager.import(date: "2020-10-04", order: 0, title: "Inktober 2020 | Radio", filename: File.join(SEED_DATA_DIR, "2020-10-04-01.jpg"))
+  .add_tags("Nightvale/Cecil")
+  .add_tags("Project/Inktober2020")
+  .add_tags("Traditional")
+
+puts "Importing 2020-10-05-01.jpg"
+PictureManager.import(date: "2020-10-05", order: 0, title: "Inktober 2020 | Blade", filename: File.join(SEED_DATA_DIR, "2020-10-05-01.jpg"))
+  .add_tags("Project/Inktober2020")
+  .add_tags("Traditional")
+
+puts "Importing 2020-10-06-01.jpg"
+PictureManager.import(date: "2020-10-06", order: 0, title: "Inktober 2020 | Rodent", filename: File.join(SEED_DATA_DIR, "2020-10-06-01.jpg"))
+  .add_tags("MLP/Sunset-Shimmer")
+  .add_tags("Project/Inktober2020")
+  .add_tags("Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Traditional")
+
+puts "Importing 2020-10-07-01.jpg"
+PictureManager.import(date: "2020-10-07", order: 0, title: "Inktober 2020 | Fancy", filename: File.join(SEED_DATA_DIR, "2020-10-07-01.jpg"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Project/Inktober2020")
+  .add_tags("Traditional")
+
+puts "Importing 2020-10-18-01.png"
+PictureManager.import(date: "2020-10-18", order: 0, title: "Roshia Ref v2", filename: File.join(SEED_DATA_DIR, "2020-10-18-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Fullcolour")
+
+puts "Importing 2020-10-20-01.png"
+PictureManager.import(date: "2020-10-20", order: 0, title: "Ensign Rarity", filename: File.join(SEED_DATA_DIR, "2020-10-20-01.png"))
+  .add_tags("MLP/Rarity")
+  .add_tags("Project/MLP-Lower-Decks")
+  .add_tags("Show/Lower-Decks", "Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Fullcolour")
+
+puts "Importing 2020-11-18-01.png"
+PictureManager.import(date: "2020-11-18", order: 0, title: "Yang Xiao Long", filename: File.join(SEED_DATA_DIR, "2020-11-18-01.png"))
+  .add_tags("RWBY/Yang")
+  .add_tags("Show/RWBY")
+  .add_tags("Fullcolour")
+
+puts "Importing 2020-12-07-01.png"
+PictureManager.import(date: "2020-12-07", order: 0, title: "Ah Hem...", filename: File.join(SEED_DATA_DIR, "2020-12-07-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Fullcolour")
+  .add_tags("Featured")
+
+puts "Importing 2020-12-19-01.png"
+PictureManager.import(date: "2020-12-19", order: 0, title: "Thank You AnimalGlitch", filename: File.join(SEED_DATA_DIR, "2020-12-19-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Project/Thank-Yous")
+  .add_tags("Fullcolour")
+
+puts "Importing 2020-12-21-01.png"
+PictureManager.import(date: "2020-12-21", order: 0, title: "Thank You JustinWolfe", filename: File.join(SEED_DATA_DIR, "2020-12-21-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Project/Thank-Yous")
+  .add_tags("Fullcolour")
+
+puts "Importing 2020-12-22-01.png"
+PictureManager.import(date: "2020-12-22", order: 0, title: "Thank You AnimeClaro", filename: File.join(SEED_DATA_DIR, "2020-12-22-01.png"))
+  .add_tags("MLP/Vinyl-Scratch")
+  .add_tags("Project/Thank-Yous")
+  .add_tags("Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Fullcolour")
+  .add_tags("Featured")
+
+puts "Importing 2020-12-25-01.png"
+PictureManager.import(date: "2020-12-25", order: 0, title: "Xmas Roshia", filename: File.join(SEED_DATA_DIR, "2020-12-25-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Christmas")
+  .add_tags("Fullcolour")
+  .add_tags("Featured")
+
+puts "Importing 2020-12-25-02.png"
+PictureManager.import(date: "2020-12-25", order: 1, title: "Xmas Sunset", filename: File.join(SEED_DATA_DIR, "2020-12-25-02.png"))
+  .add_tags("MLP/Sunset-Shimmer")
+  .add_tags("Christmas")
+  .add_tags("Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Fullcolour")
+
+puts "Importing 2020-12-26-01.png"
+PictureManager.import(date: "2020-12-26", order: 0, title: "2020 Review", filename: File.join(SEED_DATA_DIR, "2020-12-26-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("Year-Review/2020")
+  .add_tags("MLP/Sunset-Shimmer", "MLP/Vinyl-Scratch", "MLP/Applejack", "MLP/Twilight")
+  .add_tags("RWBY/Yang")
+  .add_tags("Christmas")
+  .add_tags("Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Fullcolour", "Traditional")
+  .add_tags("Featured")
+
+puts "Importing 2020-12-28-01.png"
+PictureManager.import(date: "2020-12-28", order: 0, title: "Psycho Crusher Cinder Fall | Nemsislivezx", filename: File.join(SEED_DATA_DIR, "2020-12-28-01.png"))
+  .add_tags("RWBY/Cinder-Fall")
+  .add_tags("Request/Nemsislivezx")
+  .add_tags("Show/RWBY")
+  .add_tags("Fullcolour")
+  .add_tags("That-Went-Poorly")
+
+puts "Importing 2020-12-30-01.png"
+PictureManager.import(date: "2020-12-30", order: 0, title: "Sunset Palmer | Malcontent", filename: File.join(SEED_DATA_DIR, "2020-12-30-01.png"))
+  .add_tags("MLP/Sunset-Shimmer", "CP2077/Panam-Palmer")
+  .add_tags("Request/Malcontent")
+  .add_tags("Show/My-Little-Pony", "Show/Equestira-Girls")
+  .add_tags("Game/Cyberpunk-2077")
+  .add_tags("Fullcolour")
+  .add_tags("Featured")
+
+puts "Importing 2020-12-31-01.png"
+PictureManager.import(date: "2020-12-31", order: 0, title: "Happy New Year!", filename: File.join(SEED_DATA_DIR, "2020-12-31-01.png"))
+  .add_tags("OCs/Roshia")
+  .add_tags("New-Year")
+  .add_tags("Fullcolour")
