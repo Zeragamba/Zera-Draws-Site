5dfb75582e44aab61536f1197a0d087f9794a329
Zeragamba <Zera@zeragamba.ca>
2023-01-01 12:00:56 -0500

    simplify client router

--- client/src/App/AppRouter.tsx
+++ client/src/App/AppRouter.tsx
@@ -7,51 +7,38 @@ import * as Pages from '../Pages'
 
 export const AppRouter: FC = () => {
   const userQuery = useCurrentUser()
-
   const isAdmin = !!(userQuery.data && userQuery.data.admin)
+
+  const fallbackPage = userQuery.isFetching ? (<Pages.AuthorizingPage />) : (<Navigate to={'/'} />)
 
   return (
     <BrowserRouter>
       <Routes>
-        {publicRoutes()}
-        {adminRoutes(isAdmin)}
-        {fallbackRoute(userQuery.isFetching)}
+        <>
+          <Route path="/" element={<Pages.HomePage />} />
+          <Route path="/login" element={<Pages.LoginPage />} />
+
+          <Route path="/post/:postId" element={<Pages.ViewPostPage />} />
+
+          <Route path="/tag/:tagId" element={<Pages.ViewTagPage />} />
+          <Route path="/tag/:tagId/:postId" element={<Pages.ViewPostPage />} />
+
+          <Route path="/gallery/:galleryId" element={<Pages.ViewGalleryPage />} />
+          <Route path="/gallery/:galleryId/:postId" element={<Pages.ViewPostPage />} />
+        </>
+
+        {isAdmin && (
+          <>
+            <Route path="/post/new" element={<Pages.NewPostPage />} />
+            <Route path="/post/:postId/edit" element={<Pages.EditPostPage />} />
+          </>
+        )}
+
+        <Route path="*" element={fallbackPage} />
       </Routes>
     </BrowserRouter>
   )
 }
-
-const publicRoutes = () => {
-  return (
-    <>
-      <Route path="/" element={<Pages.HomePage />} />
-      <Route path="/login" element={<Pages.LoginPage />} />
-      <Route path="/post/:postId" element={<Pages.ViewPostPage />} />
-      <Route path="/tag/:tagId/:postId" element={<Pages.ViewPostPage />} />
-      <Route path="/gallery/:galleryId/:postId" element={<Pages.ViewPostPage />} />
-    </>
-  )
-}
-
-const adminRoutes = (isAdmin: boolean) => {
-  if (!isAdmin) return null
-
-  return (
-    <>
-      <Route path="/post/new" element={<Pages.NewPostPage />} />
-      <Route path="/post/:postId/edit" element={<Pages.EditPostPage />} />
-    </>
-  )
-}
-
-const fallbackRoute = (authorizing: boolean) => {
-  const page = authorizing ? <Pages.AuthorizingPage /> : <Navigate to={'/'} />
-
-  return (
-    <Route path="*" element={page} />
-  )
-}
-
 export const useHistory = (): History => {
   return createBrowserHistory()
 }
