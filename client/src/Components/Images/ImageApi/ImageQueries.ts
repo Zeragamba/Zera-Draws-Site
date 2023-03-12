7c63f65a5f2378247b3225d86a5174a661b2b278
Zeragamba <Zera@zeragamba.ca>
2023-03-11 21:27:19 -0500

    add image data files

---
+++ client/src/Components/Images/ImageApi/ImageQueries.ts
@@ -0,0 +1,26 @@
+import { createQueryKeys } from '@lukemorales/query-key-factory'
+import { useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query'
+
+export const ImageQueries = createQueryKeys('images', {
+  get: (src: string) => ({
+    queryKey: [ src ],
+    queryFn: () => new Promise((resolve) => {
+      if (!src) return
+      const image = new Image()
+      image.src = src
+      image.onload = () => resolve(image)
+    }),
+  }),
+})
+
+export const useImage$ = (src: string | undefined): UseQueryResult<HTMLImageElement> => {
+  return useQuery({
+    enabled: !!src,
+    ...ImageQueries.get(src as string),
+  })
+}
+
+export const useImagePreload = (src: string) => {
+  useQueryClient().prefetchQuery(ImageQueries.get(src))
+}
+
