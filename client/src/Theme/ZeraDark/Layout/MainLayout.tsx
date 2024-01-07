2ad1dd8f194534d24ba808df53b9e783f6a55582
Zeragamba <code@zeragamba.ca>
2024-01-06 23:03:30 -0500

    fix overlap of navbar (#12)

--- client/src/Theme/ZeraDark/Layout/MainLayout.tsx
+++ client/src/Theme/ZeraDark/Layout/MainLayout.tsx
@@ -1,6 +1,7 @@
-import { Box, Stack, SxProps } from '@mui/material'
-import { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Box, Stack, SxProps, useMediaQuery, useTheme } from '@mui/material'
+import classnames from 'classnames'
+import { FC, useEffect, useState } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import { Sidebar } from './Sidebar'
 
@@ -11,13 +12,23 @@ const styles = {
     width: '100vw',
   },
   Sidebar: {
-    backgroundColor: 'hsla(0deg, 0%, 40%, 25%)',
+    '&.mobile': {
+      width: 56,
+      zIndex: 100,
+    },
   },
   Main: {
     flexGrow: 1,
     overflowX: 'auto',
     padding: 4,
     paddingLeft: 4,
+  },
+  Overlay: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'hsla(0deg, 0%, 0%, 15%)',
+    position: 'absolute',
+    zIndex: 90,
   },
 } satisfies Record<string, SxProps>
 
@@ -26,12 +37,30 @@ interface MainLayoutProps {
 }
 
 export const MainLayout: FC<MainLayoutProps> = () => {
+  const location = useLocation()
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'))
+  const [ sidebarOpen, setSidebarOpen ] = useState<boolean>(isSmallScreen)
+
+  useEffect(() => {
+    if (!isSmallScreen) return
+    setSidebarOpen(false)
+  }, [ location, isSmallScreen ])
+
+  useEffect(() => {
+    if (!isSmallScreen && !sidebarOpen) return setSidebarOpen(true)
+    if (isSmallScreen && sidebarOpen) return setSidebarOpen(false)
+  }, [ isSmallScreen ])
+
   return (
     <Stack sx={styles.Layout}>
-      <Box sx={styles.Sidebar}>
-        <Sidebar />
+      <Box sx={styles.Sidebar} className={classnames({ mobile: isSmallScreen })}>
+        <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
       </Box>
       <Box sx={styles.Main}>
+        {isSmallScreen && sidebarOpen && (
+          <Box sx={styles.Overlay} onClick={() => setSidebarOpen(false)} />
+        )}
         <Outlet />
       </Box>
     </Stack>
