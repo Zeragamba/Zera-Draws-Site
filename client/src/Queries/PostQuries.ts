04a6718d1c471ab76da2e2fb7acb69787455ae9e
Zeragamba <Zera@zeragamba.ca>
2024-03-25 11:21:07 -0400

    add cache for post data

--- client/src/Queries/PostQuries.ts
+++ client/src/Queries/PostQuries.ts
@@ -1,9 +1,11 @@
 import {
+  QueryClient,
   useInfiniteQuery,
   UseInfiniteQueryResult,
   useMutation,
   UseMutationResult,
   useQuery,
+  useQueryClient,
   UseQueryResult,
 } from '@tanstack/react-query'
 
@@ -21,21 +23,60 @@ export const pagedPostDataQueryOptions = {
   select: (data: { pages: PagedPostData[] }) => data.pages.map(page => page.posts).flat(),
 }
 
+function updateCache(queryClient: QueryClient, post: PostData) {
+  queryClient.removeQueries({
+    queryKey: queryKeys.posts.forPost({ postId: post.id }).queryKey,
+  })
+
+  queryClient.removeQueries({
+    queryKey: queryKeys.posts.forPost({ postId: post.slug }).queryKey,
+  })
+
+  queryClient.setQueryData(
+    queryKeys.posts.forPost({ postId: post.id })._ctx.data.queryKey,
+    post,
+  )
+
+  queryClient.setQueryData(
+    queryKeys.posts.forPost({ postId: post.slug })._ctx.data.queryKey,
+    post,
+  )
+}
+
 export const useCreatePost$ = (): UseMutationResult<PostData, unknown, {
   post: EditablePost
   images: EditableImage[]
   onUploadProgress: (progress: number) => void
 }> => {
+  const queryClient = useQueryClient()
+
   return useMutation({
-    mutationFn: (params) => postsApi.createPost(params),
+    mutationFn: async (params) => {
+      const post = await postsApi.createPost(params)
+      updateCache(queryClient, post)
+      return post
+    },
   })
 }
 
 export const useDeletePost$ = (): UseMutationResult<PostData, unknown, {
   postId: PostData['id']
 }> => {
+  const queryClient = useQueryClient()
+
   return useMutation({
-    mutationFn: (params) => postsApi.deletePost(params),
+    mutationFn: async (params) => {
+      const post = await postsApi.deletePost(params)
+
+      queryClient.removeQueries({
+        queryKey: queryKeys.posts.forPost({ postId: post.id }).queryKey,
+      })
+      queryClient.removeQueries({
+        queryKey: queryKeys.posts.forPost({ postId: post.slug }).queryKey,
+      })
+
+      return post
+    },
   })
 }
 
@@ -45,80 +86,140 @@ export const useUpdatePost$ = (): UseMutationResult<PostData, unknown, {
   changes?: ImageChangeRecord[]
   onUploadProgress?: (progress: number) => void
 }> => {
+  const queryClient = useQueryClient()
+
   return useMutation({
-    mutationFn: (params) => postsApi.updatePost(params),
+    mutationFn: async (params) => {
+      const post = await postsApi.updatePost(params)
+      updateCache(queryClient, post)
+      return post
+    },
   })
 }
 
 export const useAllPosts$ = (): UseInfiniteQueryResult<PostData[]> => {
+  const queryClient = useQueryClient()
+
   return useInfiniteQuery({
     ...pagedPostDataQueryOptions,
     ...queryKeys.posts.all,
-    queryFn: ({ pageParam }) => postsApi.fetchPosts({ page: pageParam }),
+    queryFn: async ({ pageParam }) => {
+      const page = await postsApi.fetchPosts({ page: pageParam })
+      page.posts.forEach((post) => updateCache(queryClient, post))
+      return page
+    },
   })
 }
 
 export const useRecentPosts$ = (): UseInfiniteQueryResult<PostData[]> => {
+  const queryClient = useQueryClient()
+
   return useInfiniteQuery({
     ...pagedPostDataQueryOptions,
     ...queryKeys.posts.recent,
-    queryFn: ({ pageParam }) => postsApi.fetchRecentPosts({ page: pageParam }),
+    queryFn: async ({ pageParam }) => {
+      const page = await postsApi.fetchRecentPosts({ page: pageParam })
+      page.posts.forEach((post) => updateCache(queryClient, post))
+      return page
+    },
   })
 }
 
 export const useLatestPost$ = (): UseQueryResult<PostData> => {
+  const queryClient = useQueryClient()
+
   return useQuery({
     ...queryKeys.posts.latest,
-    queryFn: () => postsApi.fetchLatestPost(),
+    queryFn: async () => {
+      const post = await postsApi.fetchLatestPost()
+      updateCache(queryClient, post)
+      return post
+    },
   })
 }
 
 export const useFirstPost$ = (): UseQueryResult<PostData> => {
+  const queryClient = useQueryClient()
+
   return useQuery({
     ...queryKeys.posts.first,
-    queryFn: () => postsApi.fetchFirstPost(),
+    queryFn: async () => {
+      const post = await postsApi.fetchFirstPost()
+      updateCache(queryClient, post)
+      return post
+    },
   })
 }
 
 export const usePost$ = (params: PostParams): UseQueryResult<PostData> => {
+  const queryClient = useQueryClient()
+
   return useQuery({
     ...queryKeys.posts.forPost(params)._ctx.data,
-    queryFn: () => postsApi.fetchPost(params),
+    queryFn: async () => {
+      const post = await postsApi.fetchPost(params)
+      updateCache(queryClient, post)
+      return post
+    },
   })
 }
 
 export const useNextPost$ = (params: PostParams): UseQueryResult<PostData> => {
+  const queryClient = useQueryClient()
+
   return useQuery({
     ...queryKeys.posts.forPost(params)._ctx.next,
-    queryFn: () => postsApi.fetchNextPost(params),
+    queryFn: async () => {
+      const post = await postsApi.fetchNextPost(params)
+      updateCache(queryClient, post)
+      return post
+    },
   })
 }
 
 export const usePrevPost$ = (params: PostParams): UseQueryResult<PostData> => {
+  const queryClient = useQueryClient()
+
   return useQuery({
     ...queryKeys.posts.forPost(params)._ctx.prev,
-    queryFn: () => postsApi.fetchPrevPost(params),
+    queryFn: async () => {
+      const post = await postsApi.fetchPrevPost(params)
+      updateCache(queryClient, post)
+      return post
+    },
   })
 }
 
 export const useGalleryPosts$ = (params: {
   galleryId: GalleryData['id']
 }): UseInfiniteQueryResult<PostData[]> => {
+  const queryClient = useQueryClient()
+
   return useInfiniteQuery({
     ...queryKeys.posts.inGallery(params)._ctx.data,
-    queryFn: ({ pageParam }) => postsApi.fetchGalleryPosts({...params, page: pageParam }),
-    select: (data) => data.pages.map(page => page.posts).flat(),
     ...pagedQueryOptions,
+    queryFn: async ({ pageParam }) => {
+      const page = await postsApi.fetchGalleryPosts({...params, page: pageParam })
+      page.posts.forEach((post) => updateCache(queryClient, post))
+      return page
+    },
+    select: (data) => data.pages.map(page => page.posts).flat(),
   })
 }
 
 export const useTaggedPosts$ = (params: {
   tagId: TagData['id']
 }): UseInfiniteQueryResult<PostData[]> => {
+  const queryClient = useQueryClient()
+
   return useInfiniteQuery({
     ...queryKeys.posts.withTag(params)._ctx.data,
-    queryFn: ({ pageParam }) => postsApi.fetchTaggedPosts({...params, page: pageParam }),
-    select: (data) => data.pages.map(page => page.posts).flat(),
     ...pagedQueryOptions,
+    queryFn: async ({ pageParam }) => {
+      const page = await postsApi.fetchTaggedPosts({...params, page: pageParam })
+      page.posts.forEach((post) => updateCache(queryClient, post))
+      return page
+    },
+    select: (data) => data.pages.map(page => page.posts).flat(),
   })
 }
