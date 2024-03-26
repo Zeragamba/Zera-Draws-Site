66b6b690e77d9c55eac551ce5e965f7ecb97befb
Zeragamba <Zera@zeragamba.ca>
2024-03-26 10:04:14 -0400

    remove misc logs

--- client/src/Queries/QueryUtils.ts
+++ client/src/Queries/QueryUtils.ts
@@ -3,12 +3,10 @@ import { PagedResMetaData } from '../Api/Schemas/PagedResMetaDataSchema'
 export const pagedQueryOptions = {
   initialPageParam: 0,
   getNextPageParam: (lastPage: PagedResMetaData) => {
-    console.log({ page: lastPage.page, total: lastPage.total_pages })
     if (lastPage.page >= lastPage.total_pages) return null
     return lastPage.page + 1
   },
   getPreviousPageParam: (lastPage: PagedResMetaData) => {
-    console.log({ page: lastPage.page, total: lastPage.total_pages })
     if (lastPage.page <= 0) return null
     return lastPage.page - 1
   },
