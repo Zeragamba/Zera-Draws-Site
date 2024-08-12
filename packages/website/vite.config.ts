38d9c5784c396ef84accf3705289cd0fd1e8ad80
Zeragamba <code@Zeragamba.ca>
2024-08-11 23:16:47 -0400

    enable dev https

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ServerOptions } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as fs from 'node:fs'
+
+const server: ServerOptions = {
+  host: true,
+  port: 3000,
+}
+
+if (process.env.HTTPS === 'true') {
+  const keyFile = process.env.SSL_KEY_FILE
+  if (!keyFile) throw new Error('SSL_KEY_FILE not set')
+
+  const crtFile = process.env.SSL_CRT_FILE
+  if (!crtFile) throw new Error('SSL_CRT_FILE not set')
+
+  server.https = {
+    key: fs.readFileSync(keyFile),
+    cert: fs.readFileSync(crtFile),
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react() ],
-  server: {
-    host: true,
-    port: 3000,
-  },
+  server: server,
 })
