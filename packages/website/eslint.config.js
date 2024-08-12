dbfd764a7cb0f712f39dd2d24bd265a7a617eb2d
Zeragamba <code@Zeragamba.ca>
2024-08-11 23:16:47 -0400

    update eslint config

--- packages/website/eslint.config.js
+++ packages/website/eslint.config.js
@@ -22,5 +22,9 @@ export default tseslint.config({
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': ['error', {
+      varsIgnorePattern: '^_',
+      argsIgnorePattern: '^_',
+    }],
   },
 })
