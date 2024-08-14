9117ab08f195ae01e394e029aa169a7fcf117c77
Zeragamba <code@Zeragamba.ca>
2024-08-13 20:03:23 -0400

    reformat provider list

--- packages/client/src/App/AppProviders.tsx
+++ packages/client/src/App/AppProviders.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@mui/material"
 import { LocalizationProvider } from "@mui/x-date-pickers"
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns"
 import { QueryClientProvider } from "@tanstack/react-query"
-import { FC, PropsWithChildren, useMemo } from "react"
+import { FC, PropsWithChildren, ReactNode } from "react"
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
 
@@ -16,33 +16,19 @@ type AppProvidersProps = PropsWithChildren<{
 }>
 
 export const AppProviders: FC<AppProvidersProps> = ({ theme, children }) => {
-  const providers = useMemo<FC<PropsWithChildren>[]>(
-    () => [
-      ({ children }) => (
-        <ThemeProvider theme={theme.muiTheme} children={children} />
-      ),
-      ({ children }) => (
-        <LocalizationProvider
-          dateAdapter={AdapterDateFns}
-          children={children}
-        />
-      ),
-      ({ children }) => (
-        <QueryClientProvider client={queryClient} children={children} />
-      ),
-      ({ children }) => <AgeGateProvider children={children} />,
-      ({ children }) => <PreloadProvider children={children} />,
-      ({ children }) => (
-        <DndProvider backend={HTML5Backend} children={children} />
-      ),
-    ],
-    [theme],
-  )
+  // prettier-ignore
+  const providers: ((children: ReactNode) => ReactNode)[] = [
+    (children) => <ThemeProvider theme={theme.muiTheme} children={children} />,
+    (children) => <LocalizationProvider dateAdapter={AdapterDateFns} children={children} />,
+    (children) => <QueryClientProvider client={queryClient} children={children} />,
+    (children) => <AgeGateProvider children={children} />,
+    (children) => <PreloadProvider children={children} />,
+    (children) => <DndProvider backend={HTML5Backend} children={children} />
+  ]
 
   let stack = children
-
-  for (const Provider of providers.toReversed()) {
-    stack = <Provider children={stack} />
+  for (const provider of providers.toReversed()) {
+    stack = provider(stack)
   }
 
   return stack
